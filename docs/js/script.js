e7de0acd39a30c0e6399bab79797ec7518a44e25
Timothée MARRERO MEYER <143833750+timotheeMM@users.noreply.github.com>
2023-09-04 18:10:12 +0200

    improve dark mode

--- docs/js/script.js
+++ docs/js/script.js
@@ -60,10 +60,12 @@ changeThemeButton.addEventListener("click", () => {
     if(toggleTheme === 0) {
         document.documentElement.style.setProperty("--background-color-and-button-text", "#fff");
         document.documentElement.style.setProperty("--background-button", "#222");
+        document.documentElement.style.setProperty("--invert", "0%");
         toggleTheme++;
     } else {
         document.documentElement.style.setProperty("--background-color-and-button-text", "#222");
         document.documentElement.style.setProperty("--background-button", "#fff");
+        document.documentElement.style.setProperty("--invert", "100%");
         toggleTheme--;
     }
 })
